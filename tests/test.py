4080a26b0669669b924fc9523e3e2cd4cd5772e7
pengchzn <pengchzn@gmail.com>
2022-02-22 16:30:05 +0800

    Update the test.py

--- tests/test.py
+++ tests/test.py
@@ -1,63 +1,59 @@
-import popcorn
-import numpy as np
-import scipy.sparse.linalg as sp
-from numpy.linalg import inv
+import hdbscan
+import pandas as pd
+from matplotlib import pyplot as plt
 
+from popcorn.popcorn import popcorn
 
-def popcorn(X, Xsd, p, MaxIter=1e5):
-    X = np.mat(X)
-    m = X.shape[0]
-    n = X.shape[1]
-    if p > min(m, n):
-        exit("popcorn:err1 - Invalid rank for MLPCA decomposition")
-    ml = Xsd.shape[0]
-    nl = Xsd.shape[1]
-    if m != ml or n != nl:
-        exit("popcorn:err2 - Dimensions of data and standard deviations do not matchn")
-    if bool(np.any(Xsd == 0)):
-        exit("popcorn:err3 - Zero value(s) for standard deviations")
-    ConvLim = 1e-10
-    MaxIter = MaxIter
-    VarMult = 1000
-    VarX = np.multiply(Xsd, Xsd)
-    VarX[np.isnan(VarX)] == VarX.max() * VarMult
-    U, S, V = sp.svds(X, k=p)
-    Count = 0
-    Sold = 0
-    ErrFlag = -1
-    while (ErrFlag < 0):
-        Count = Count + 1
-        Sobj = 0
-        MLX = np.mat(np.zeros((X.shape[0], X.shape[1])))
-        for j in range(0, n):
-            Q = np.diagflat(1 / VarX[:, j])
-            FInter = inv(U.T @ Q @ U)
-            MLX[:, j] = U @ (FInter @ (U.T @ (Q @ X[:, j])))
-            Dx = np.mat(X[:, j] - MLX[:, j])
-            Sobj = Sobj + Dx.T @ Q @ Dx
-        if Count % 2 == 1:
-            ConvCalc = np.abs(Sold - Sobj) / Sobj
-            if ConvCalc < ConvLim:
-                ErrFlag = 0
-            if Count > MaxIter:
-                ErrFlag = 1
-                exit("popcorn:err4 - Maximum iterations exceeded")
-        if ErrFlag < 0:
-            Sold = Sobj
-            U, S, V = sp.svds(MLX, k=p)
-            V = V.T
-            X = X.T
-            VarX = VarX.T
-            n = X.shape[1]
-            U = V
-    U, S, V = sp.svds(MLX, k=p)
-    S = np.mat(np.diag(S))
-    V = V.T
-    return U, S, V
+X = pd.read_csv('./datasets/Xobs.csv')
+Xsd = pd.read_csv('./datasets/Xsd.csv')
 
+# Plot the distribution of original data
+plt.scatter('V1', 'V2', s=10, data=X)
+plt.title('Original data')
+plt.xlim((-0.4, 0.4))
+plt.ylim((-0.3, 0.3))
+plt.show()
 
-X = np.loadtxt('./datasets/Xobs.csv', delimiter=",", skiprows=1)
-Xsd = np.loadtxt('./datasets/Xsd.csv', delimiter=",", skiprows=1)
-
+# run the POPCORN
 U, S, V = popcorn(X, Xsd, 2)
 result = U @ S @ V.T
+result = pd.DataFrame(result)
+column = []
+for i in range(1, 21):
+    column.append("V" + str(i))
+result.columns = column
+
+# Plot the distribution of denoised data
+plt.scatter('V1', 'V2', s=10, data=result)
+plt.title('Denoised data')
+plt.xlim((-0.4, 0.4))
+plt.ylim((-0.3, 0.3))
+plt.show()
+
+# Use the HDBScan to do the classification on the original data
+clusterer = hdbscan.HDBSCAN(min_cluster_size=5, gen_min_span_tree=True)
+clusterer.fit(X[['V1', 'V2']])
+X['label'] = clusterer.labels_
+X['proab'] = clusterer.probabilities_
+X.loc[X['proab'] < 0.5, 'label'] = -1
+plt.xlabel('V1', fontsize=12)
+plt.ylabel('V2', fontsize=12)
+plt.title('Classification on nosy data')
+plt.scatter('V1', 'V2', c=X['label'], s=10, cmap='plasma', data=X)
+plt.xlim((-0.4, 0.4))
+plt.ylim((-0.3, 0.3))
+plt.show()
+
+# # Use the HDBScan to do the classification on the denoised data
+clusterer2 = hdbscan.HDBSCAN(min_cluster_size=5, gen_min_span_tree=True)
+clusterer2.fit(result[['V1', 'V2']])
+result['label'] = clusterer2.labels_
+result['proab'] = clusterer2.probabilities_
+result.loc[result['proab'] < 0.5, 'label'] = -1
+plt.xlabel('V1', fontsize=12)
+plt.ylabel('V2', fontsize=12)
+plt.title('Classify on denoised data')
+plt.scatter('V1', 'V2', c=result['label'], s=10, cmap='plasma', data=result)
+plt.xlim((-0.4, 0.4))
+plt.ylim((-0.3, 0.3))
+plt.show()
