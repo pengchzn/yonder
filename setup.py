2234af8b48347d7c373829d5df9868bc95803740
pengchzn <pengchzn@gmail.com>
2022-02-25 13:54:49 +0800

    update the setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
-with open('README.md', 'r') as fp:
+with open('README.rst', 'r') as fp:
     long_description = fp.read()
 
 setuptools.setup(
     name='yonder',
     version='1.0',
-    description='A pYthON package for Data dEnoising and Reconstruction',
+    description='A python package for data denoising and reconstruction',
     long_description=long_description,
     long_description_content_type='text/x-rst',
     url='https://github.com/pengchzn/yonder',
     keywords=['Astrostatistics techniques', 'Astronomy software', 'Astronomy data analysis'],
-    author='Rafael S. de Souza, Peng Chen',
-    author_email='drsouza@shao.ac.cn, pengchzn@gmail.com',
+    author='Peng Chen',
+    author_email='pengchzn@gmail.com',
     packages=setuptools.find_packages(),
     classifiers=[
         'Topic :: Scientific/Engineering',
-        'License :: OSI Approved :: GPL License',
+        'License :: OSI Approved :: GNU General Public License v3.0',
         'Programming Language :: Python :: 3',
     ],
 )
